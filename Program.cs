5f8ea0d97b5a866bcf5e499e481f6981c908a6a2
my pc <my pc@DESKTOP-SGIKIS6>
2022-02-20 18:13:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace StringEX
+{
+    class Xyz
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a string");
+            string str = Console.ReadLine();
+            string reverse = "";
+
+            int length = str.Length - 1;
+            while (length >= 0)
+            {
+                reverse = reverse + str[length];
+                length--;
+            }
+            if (str == reverse)
+                Console.WriteLine("palindrome");
+            else
+                Console.WriteLine("not a palindrome");
+
+
+            Console.ReadLine();
+        }
+    }
+}
